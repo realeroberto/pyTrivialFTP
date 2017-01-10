71f16af59258b42e1ffdf77959ab7b49d51b238b
Roberto Reale <roberto.reale@linux.com>
2017-01-10 11:58:18 +0100

    Updated author's email.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description = "The poor man's API for accessing data on a remote FTP repository",
     py_modules = [ 'pyTrivialFTP' ],
     author = 'Roberto Reale',
-    author_email = 'roberto.reale82@gmail.com',
+    author_email = 'roberto.reale@linux.com',
     url = 'https://github.com/robertoreale/pyTrivialFTP',
     keywords = [ 'FTP' ],
     install_requires = [ 
