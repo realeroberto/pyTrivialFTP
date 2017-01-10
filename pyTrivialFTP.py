01f32eff82b6248927f68f71637eb632b8e1f402
Roberto Reale <roberto.reale@linux.com>
2017-01-10 12:00:29 +0100

    Removed author's email from pyTrivialFTP.py.

--- pyTrivialFTP.py
+++ pyTrivialFTP.py
@@ -3,7 +3,7 @@
 
 # The MIT License (MIT)
 #
-# Copyright (c) 2014-7 Roberto Reale <roberto.reale82@gmail.com>
+# Copyright (c) 2014-7 Roberto Reale
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
