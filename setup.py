3e7ab2c2d747c37ba2097b7e08c863348bb186a8
Roberto Reale <roberto.reale@linux.com>
2017-01-10 11:57:27 +0100

    Updated package email in setup.py.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     py_modules = [ 'pyTrivialFTP' ],
     author = 'Roberto Reale',
     author_email = 'roberto.reale82@gmail.com',
-    url = 'https://github.com/roberto-reale/pyTrivialFTP',
+    url = 'https://github.com/robertoreale/pyTrivialFTP',
     keywords = [ 'FTP' ],
     install_requires = [ 
     'FTP',
