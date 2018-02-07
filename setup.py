3f151ca6156db8423a27d235fc0e820103509fda
Roberto Reale <rober.reale@gmail.com>
2018-02-07 18:38:47 +0100

    Improve setup.py.

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ setup(
     name = 'pyTrivialFTP',
     version = '0.2.0',
     description = "The poor man's API for accessing data on a remote FTP repository",
-    py_modules = [ 'pyTrivialFTP' ],
+    packages = [ 'pyTrivialFTP' ],
     author = 'Roberto Reale',
-    author_email = 'roberto.reale@linux.com',
+    author_email = 'rober.reale@gmail.com',
     url = 'https://github.com/robertoreale/pyTrivialFTP',
     keywords = [ 'FTP' ],
-    install_requires = [ 
-    'FTP',
-    ]
+    install_requires = [ 'FTP', ],
+    test_suite = 'nose.collector',
+    tests_require = ['nose'],
 )
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
